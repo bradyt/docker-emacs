5c2e8e1e2d70e160796f5774d1904c0bc41c7330
Brady Trainor <mail@bradyt.net>
2020-03-18 06:51:38 -0700

    Cleanup inits

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,2 @@
 
-(require 'package)
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
-
-(setq dart-debug t)
-
 (setq lsp-dart-sdk-dir "/usr/lib/dart/")
-
-(let ((packages-to-install
-       (seq-remove 'package-installed-p
-                   '(dart-mode
-                     dart-server))))
-  (when packages-to-install
-    (package-refresh-contents)
-    (dolist (package packages-to-install)
-      (package-install package))))
--- .emacs.d/install.el
+++ .emacs.d/install.el
@@ -1,16 +1,11 @@
 
-(require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
 
-(let ((packages-to-install
-       (seq-remove 'package-installed-p
-                   '(dart-mode
-                     lsp-mode
-                     dart-server))))
-  (when packages-to-install
-    (package-refresh-contents)
-    (dolist (package packages-to-install)
-      (package-install package))))
+(package-refresh-contents)
+
+(dolist (pkg '(dart-mode
+               lsp-mode
+               dart-server))
+  (package-install pkg))
